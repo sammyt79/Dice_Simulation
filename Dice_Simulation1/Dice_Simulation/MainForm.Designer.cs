6445556c66f977798f80eaa71a1325bc136c483b
Samuel Tollefson <sam8879@gmail.com>
2015-05-07 00:24:44 -0400

    .NET2

---
+++ Dice_Simulation1/Dice_Simulation/MainForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Dice_Simulation
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.lstResults = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.Font = new System.Drawing.Font("Baskerville Old Face", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRoll.Location = new System.Drawing.Point(89, 12);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(97, 49);
+            this.btnRoll.TabIndex = 0;
+            this.btnRoll.Text = "Roll the dice";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            this.btnRoll.Click += new System.EventHandler(this.btnRoll_Click);
+            // 
+            // lstResults
+            // 
+            this.lstResults.FormattingEnabled = true;
+            this.lstResults.Location = new System.Drawing.Point(21, 67);
+            this.lstResults.Name = "lstResults";
+            this.lstResults.Size = new System.Drawing.Size(238, 173);
+            this.lstResults.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lstResults);
+            this.Controls.Add(this.btnRoll);
+            this.Name = "MainForm";
+            this.Text = "Dice Simulation";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRoll;
+        private System.Windows.Forms.ListBox lstResults;
+    }
+}
+
---
+++ Dice_Simulation1/Dice_Simulation/MainForm.cs
@@ -0,0 +1,79 @@
+﻿// Dice Simulation
+// Samuel Tollefson
+// POS/409
+// April 6, 2015
+// Jon Jensen
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dice_Simulation
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Tell the user about the program.
+            MessageBox.Show("This program simulates the rolling of two dice 100 times." 
+                + Environment.NewLine
+                + Environment.NewLine
+                + "Whenever the two dice display the same number, " 
+                + "the program will show the roll sequence number and the number on the dice.");
+        }
+
+        private void btnRoll_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random(); // Initiate a random object.
+            int dice1 = 0; // Variable to hold the value of the first dice.
+            int dice2 = 0; // Variable to hold the value of the second dice.
+            int inaRow = 0; // Variable to hold the number of sequential double rolls.
+
+            lstResults.Items.Clear(); // Clear the list
+            
+            // Count up to 100 in increments of 1.
+            for (int sequence = 1; sequence <= 100; sequence ++)
+            {
+                dice1 = rnd.Next(1, 7); // Create a number between 1 and 6.
+                dice2 = rnd.Next(1, 7); // Create a number between 1 and 6.
+                if (dice1 == dice2) // Compare the numbers.
+                {
+                    inaRow = inaRow + 1; // Increment sequential double rolls.
+
+                    // Display the rolls that produced doubles.
+                    lstResults.Items.Add("Roll " + sequence + " produced a pair of " + dice1 + "s");
+
+                    // This if statement was added because the following else statement displays the sequential doubles 
+                    // only after the sequential doubles are finished. If the doubles end on the final roll,
+                    // there is no other roll to display the number of doubles rolled.
+                    if (inaRow > 1 & sequence == 100)
+                    {
+                        // Display the number of sequential double rolls.
+                        lstResults.Items.Add("Nice, " + inaRow + " in a row");
+                    }
+                }
+                else
+                {
+                    // Check to see if doubles have been rolled at least twice in a row.
+                    if (inaRow > 1)
+                    {
+                        // Display the number of sequential double rolls.
+                        lstResults.Items.Add("Nice, " + inaRow + " in a row");
+                    }
+                    inaRow = 0; // If doubles are not rolled, reset the variable.
+                }
+            }
+        }
+    }
+}
